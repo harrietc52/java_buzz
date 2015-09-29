53a027c18e1b29479c1243d24fe9606a7a5ef9f9
Usman Jamil <usman.jamil@live.co.uk>
2015-09-29 16:30:49 +0100

    added first function

--- spec/JavabuzzSpec.js
+++ spec/JavabuzzSpec.js
@@ -6,9 +6,7 @@ describe("Javabuzz", function() {
 
     it('divisible by 3', function() {
       javabuzz = new Javabuzz();
-      expect(isDivisibleByThree(3)).toBe(true);
+      expect(javabuzz.isDivisibleByThree(3)).toBe(true);
     });
-
   });
-
 });
--- src/Javabuzz.js
+++ src/Javabuzz.js
@@ -1,2 +1,5 @@
-function Javabuzz() {
-}
+var Javabuzz = function() {};
+
+Javabuzz.prototype.isDivisibleByThree = function(number) {
+  return true;
+};
