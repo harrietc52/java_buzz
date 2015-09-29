4944cb89932d8f09bad7072f51678849acb3f6ed
Harriet Craven <harrietc52@hotmail.co.uk>
2015-09-29 16:45:15 +0100

    test added to divisible by 3

--- spec/JavabuzzSpec.js
+++ spec/JavabuzzSpec.js
@@ -1,12 +1,22 @@
 describe("Javabuzz", function() {
 
-  var javabuzz;
+  beforeEach(function() {
+    javabuzz = new Javabuzz;
+  });
 
   describe('knows when a number is', function() {
 
     it('divisible by 3', function() {
-      javabuzz = new Javabuzz();
       expect(javabuzz.isDivisibleByThree(3)).toBe(true);
     });
   });
+
+  describe('knows when a number is not', function() {
+
+    it('divisible by 3', function() {
+      expect(javabuzz.isDivisibleByThree(1)).toBe(false);
+    });
+
+  });
+
 });
--- src/Javabuzz.js
+++ src/Javabuzz.js
@@ -1,5 +1,5 @@
 var Javabuzz = function() {};
 
-Javabuzz.prototype.isDivisibleByThree = function(number) {
-  return true;
-};
+  Javabuzz.prototype.isDivisibleByThree = function(number) {
+    return (number % 3 === 0);
+  };
