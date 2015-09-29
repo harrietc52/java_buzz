c3330beb49b5d19173f5449ed2099c59ca001b3b
Harriet Craven <harrietc52@hotmail.co.uk>
2015-09-29 17:30:20 +0100

    refractoring

--- spec/JavabuzzSpec.js
+++ spec/JavabuzzSpec.js
@@ -1,5 +1,7 @@
 describe("Javabuzz", function() {
 
+  var javabuzz;
+
   beforeEach(function() {
     javabuzz = new Javabuzz;
   });
@@ -9,12 +11,29 @@ describe("Javabuzz", function() {
     it('divisible by 3', function() {
       expect(javabuzz.isDivisibleByThree(3)).toBe(true);
     });
+
+    it('divisible by 5', function() {
+      expect(javabuzz.isDivisibleByFive(5)).toBe(true);
+    });
+
+    it('divisible by 15', function() {
+      expect(javabuzz.isDivisibleByFifteen(15)).toBe(true);
+    });
+
   });
 
   describe('knows when a number is not', function() {
 
     it('divisible by 3', function() {
       expect(javabuzz.isDivisibleByThree(1)).toBe(false);
+    });
+
+    it('divisible by 5', function() {
+      expect(javabuzz.isDivisibleByFive(1)).toBe(false);
+    });
+
+    it('divisible by 15', function() {
+      expect(javabuzz.isDivisibleByFifteen(1)).toBe(false);
     });
 
   });
