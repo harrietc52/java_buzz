514d5998b787b72bac29344e9a61ae548c24714e
Harriet Craven <harrietc52@hotmail.co.uk>
2015-09-29 17:42:21 +0100

    completed javabuzz tutorial

--- src/Javabuzz.js
+++ src/Javabuzz.js
@@ -15,3 +15,16 @@ var Javabuzz = function() {};
   Javabuzz.prototype.isDivisibleByFifteen = function(number) {
     return this._isDivisibleBy(number, 15);
   };
+
+  Javabuzz.prototype.says = function(number) {
+    if (this.isDivisibleByFifteen(number)) {
+      return "Javabuzz";
+    }
+    if (this.isDivisibleByThree(number)) {
+      return "Java";
+    }
+    if (this.isDivisibleByFive(number)) {
+      return "Buzz";
+    }
+    return number;
+  };
